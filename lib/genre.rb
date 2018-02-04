1a5081adc24a4d5450b2c03e4d81963d92889c74
Derek Haller <dhaller.personal@gmail.com>
2018-02-04 06:19:09 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -2,6 +2,9 @@ class Genre
   attr_accessor :name, :artists, :songs
   def initialize(name)
     @name = name
-    @songs ||= []
+    @songs = []
+  end
+  def songs(song)
+    self.songs << song
   end
 end
